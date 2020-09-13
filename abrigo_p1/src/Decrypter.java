7aec889c11d4d23da46b73666d9e1463593d888d
jonabr10 <jonabrigo@knights.ucf.edu>
2020-09-12 22:00:54 -0400

    Second commit

--- abrigo_p1/src/Decrypter.java
+++ abrigo_p1/src/Decrypter.java
@@ -1,2 +1,41 @@
 public class Decrypter {
+    private String origMessage;
+
+    public String decrypt(String secretMessage) {
+        int[] numValOfsecretMsg = new int[secretMessage.length()];
+        char[] charValOfsecretMsg = new char[secretMessage.length()];
+
+        for (int i = 0; i < secretMessage.length(); i++) {
+            numValOfsecretMsg[i] = secretMessage.charAt(i) - '0';
+
+            if (numValOfsecretMsg[i] >= 7) {
+                numValOfsecretMsg[i] = (numValOfsecretMsg[i] - 7) % 10;
+            } else {
+                numValOfsecretMsg[i] = (numValOfsecretMsg[i] + 10) % 7;
+            }
+        }
+
+        doIndexSwap(numValOfsecretMsg);
+
+        for (int j = 0; j < secretMessage.length(); j++) {
+            charValOfsecretMsg[j] = (char) (numValOfsecretMsg[j] + '0');
+        }
+
+        origMessage = new String(charValOfsecretMsg);
+        return origMessage;
+    }
+
+    public void doIndexSwap(int[] array) {
+        int temp;
+
+        // Note: swap values inside index 0 and index 2
+        temp = array[0];
+        array[0] = array[2];
+        array[2] = temp;
+
+        // Note: swap values inside index 1 and index 3
+        temp = array[1];
+        array[1] = array[3];
+        array[3] = temp;
+    }
 }
--- abrigo_p1/src/Encrypter.java
+++ abrigo_p1/src/Encrypter.java
@@ -1,2 +1,38 @@
 public class Encrypter {
+    private String secretMessage;
+
+    public String encrypt(String newMessage) {
+
+        int[] numValOfnewMsg = new int[newMessage.length()];
+        char[] charValOfnewMsg = new char[newMessage.length()];
+
+        for (int i = 0; i < newMessage.length(); i++) {
+            numValOfnewMsg[i] = newMessage.charAt(i) - '0';
+            numValOfnewMsg[i] = (numValOfnewMsg[i] + 7) % 10;
+        }
+
+        doIndexSwap(numValOfnewMsg);
+
+        for (int j = 0; j < newMessage.length(); j++) {
+            charValOfnewMsg[j] = (char) (numValOfnewMsg[j] + '0');
+        }
+
+        secretMessage = new String(charValOfnewMsg);
+        return secretMessage;
+    }
+
+    public void doIndexSwap(int[] array) {
+        int temp;
+
+        // Note: swap values inside index 0 and index 2
+        temp = array[0];
+        array[0] = array[2];
+        array[2] = temp;
+
+        // Note: swap values inside index 1 and index 3
+        temp = array[1];
+        array[1] = array[3];
+        array[3] = temp;
+   }
 }
+
