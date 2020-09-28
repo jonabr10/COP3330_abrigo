9650cc95e9e21342e9a86ad8b75f3e84f95fa704
jonabr10 <jonabrigo@knights.ucf.edu>
2020-09-28 01:11:51 -0400

    second commit w/ working program

--- abrigo_p2/src/App.java
+++ abrigo_p2/src/App.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
 public class App {
     public static void main(String[] args) {
         ArrayList<BodyMassIndex> bmiData = new ArrayList<BodyMassIndex>();
@@ -14,4 +18,61 @@ public class App {
 
         displayBmiStatistics(bmiData);
     }
+
+    // Criteria 4: 2pts
+    public static boolean moreInput() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Would you like to input a BMI data? (Y or N)");
+        String choice = input.nextLine();
+
+        // Note: Just to take into account an upper/lowercase 'Y'
+        choice = choice.toLowerCase();
+
+        return choice.equals("y");
+    }
+
+    // Criteria 5: 2pts
+    public static double getUserHeight() {
+        Scanner input = new Scanner(System.in);
+        double height = 0.00;
+
+        while (height <= 0) {
+            System.out.println("Input Height (positive values only):");
+            height = input.nextDouble();
+        }
+
+        return height;
+    }
+
+    // Criteria 6: 2pts
+    public static double getUserWeight() {
+        Scanner input = new Scanner(System.in);
+        double weight = 0.00;
+
+        while (weight <= 0) {
+            System.out.println("Input Weight (positive values only):");
+            weight = input.nextDouble();
+        }
+
+        return weight;
+    }
+
+    // Criteria 7: 2pts
+    public static void displayBmiInfo(BodyMassIndex bmi) {
+        System.out.println("BMI score: " + bmi.getformattedBMI() + " " + bmi.calculateCategory());
+    }
+
+    // Criteria 8: 2pts
+    public static void displayBmiStatistics(ArrayList<BodyMassIndex> bmiData) {
+        double sum = 0;
+
+        for (BodyMassIndex x: bmiData) {
+            sum += x.getBMI();
+        }
+
+        System.out.printf("Average: %.2f", (sum / bmiData.size()));
+    }
+
 }
+
+
---
+++ abrigo_p2/src/BodyMassIndex.java
@@ -0,0 +1,38 @@
+import java.text.DecimalFormat;
+
+public class BodyMassIndex {
+    private double bmi;
+
+    public BodyMassIndex(double height, double weight) {
+        this.bmi = calculateBMI(height, weight);
+    }
+
+    // Criteria 9: 2pts
+    public double calculateBMI(double height, double weight) {
+        return 703 * weight / Math.pow(height, 2);
+    }
+
+    // Criteria 10: 2pts
+    public String calculateCategory() {
+        if (bmi < 18.5) {
+            return "(Underweight < 18.5)";
+        } else if (bmi <= 24.9) {
+            return "(Normal weight = 18.5 - 24.9)";
+        } else if (bmi <= 29.9) {
+            return "(Overweight = 25 - 29.9)";
+        } else {
+            return "(Obesity >= 30.0)";
+        }
+    }
+
+    public double getBMI() {
+        return bmi;
+    }
+
+    public String getformattedBMI() {
+        String pattern = "#.##";
+        DecimalFormat decimalFormat = new DecimalFormat(pattern);
+
+        return decimalFormat.format(bmi);
+    }
+}
