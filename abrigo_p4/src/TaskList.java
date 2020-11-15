0e280e3f7d262ab8ad216793ec03fad1513dec55
jonabr10 <jonabrigo@knights.ucf.edu>
2020-11-15 03:29:06 -0500

    TaskList almost done, just needs to add methods for loading and writing to text files

--- abrigo_p4/src/TaskList.java
+++ abrigo_p4/src/TaskList.java
@@ -1,3 +1,159 @@
+import java.util.*;
+
 public class TaskList {
-    // utilize local date object for dates!
+    ArrayList<TaskItem> tasks;
+
+    public TaskList() {
+        tasks = new ArrayList<>();
+    }
+
+    public void addTask(TaskItem data) {
+        tasks.add(data);
+    }
+
+    public void removeTask(int index) {
+        try {
+            tasks.remove(index);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Error: task list at index " + index + " does not exist.");
+            throw new IndexOutOfBoundsException();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void editTask(int index, String newTitle, String newDesc, String newDate) {
+        try {
+            TaskItem temp = tasks.get(index);
+
+            //System.out.print("Enter a new title for task " + index + ": ");
+            temp.setTitle(newTitle);
+
+            //System.out.println("Enter a new description for task " + index + ": ");
+            temp.setDescription(newDesc);
+
+            //System.out.println("Enter a new task due date (MM/DD/YYYY) for task " + index + ": ");
+            temp.setDate(newDate);
+
+            tasks.set(index, temp);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Error: task list at index " + index + " does not exist.");
+            throw new IndexOutOfBoundsException();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void editTaskCompletionStatusToDone(int index) {
+        try {
+            TaskItem temp = tasks.get(index);
+            temp.setCompletionStatusToDone();
+            tasks.set(index, temp);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Error: task list at index " + index + " does not exist.");
+            throw new IndexOutOfBoundsException();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void editTaskCompletionStatusToNotDone(int index) {
+        try {
+            TaskItem temp = tasks.get(index);
+            temp.setCompletionStatusToNotDone();
+            tasks.set(index, temp);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Error: task list at index " + index + " does not exist.");
+            throw new IndexOutOfBoundsException();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void editTaskTitle(int index, String newTitle) {
+        try {
+            TaskItem temp = tasks.get(index);
+            temp.setTitle(newTitle);
+            tasks.set(index, temp);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Error: task list at index " + index + " does not exist.");
+            throw new IndexOutOfBoundsException();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void editTaskDescription(int index, String newDesc) {
+        try {
+            TaskItem temp = tasks.get(index);
+            temp.setDescription(newDesc);
+            tasks.set(index, temp);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Error: task list at index " + index + " does not exist.");
+            throw new IndexOutOfBoundsException();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void editTaskDate(int index, String newDate) {
+        try {
+            TaskItem temp = tasks.get(index);
+            temp.setDate(newDate);
+            tasks.set(index, temp);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Error: task list at index " + index + " does not exist.");
+            throw new IndexOutOfBoundsException();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getSize() {
+        return tasks.size();
+    }
+    public boolean getItemCompletionStatus(int index) {
+        if (isItemValid(index)) {
+            TaskItem temp = tasks.get(index);
+            return temp.getCompletionStatus();
+        } else {
+            System.out.println("Error: task list at index " + index + " does not exist.");
+            throw new IndexOutOfBoundsException();
+        }
+    }
+    public String getItemTitle(int index) {
+        if (isItemValid(index)) {
+            TaskItem temp = tasks.get(index);
+            return temp.getTitle();
+        } else {
+            System.out.println("Error: task list at index " + index + " does not exist.");
+            throw new IndexOutOfBoundsException();
+        }
+    }
+    public String getItemDescription(int index) {
+        if (isItemValid(index)) {
+            TaskItem temp = tasks.get(index);
+            return temp.getDescription();
+        } else {
+            System.out.println("Error: task list at index " + index + " does not exist.");
+            throw new IndexOutOfBoundsException();
+        }
+    }
+    public String getItemDate(int index) {
+        if (isItemValid(index)) {
+            TaskItem temp = tasks.get(index);
+            return temp.getDate();
+        } else {
+            System.out.println("Error: task list at index " + index + " does not exist.");
+            throw new IndexOutOfBoundsException();
+        }
+    }
+    public TaskItem getItem(int index) {
+        if (isItemValid(index)) {
+            return tasks.get(index);
+        } else {
+            System.out.println("Error: task list at index " + index + " does not exist.");
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public boolean isItemValid(int index) {
+        return index <= tasks.size();
+    }
+
 }
---
+++ abrigo_p4/src/TaskListTest.java
@@ -0,0 +1,222 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskListTest {
+    @Test
+    public void newTaskListIsEmpty() {
+        TaskList newList = new TaskList();
+        assertEquals(0, newList.getSize());
+    }
+
+    @Test
+    public void addingTaskItemsIncreasesSize() {
+        TaskItem newItem1 = new TaskItem("Title", "Description", "01/10/2020");
+        TaskItem newItem2 = new TaskItem("Title", "Description", "01/10/2020");
+        TaskList newList = new TaskList();
+
+        newList.addTask(newItem1);
+        assertEquals(1, newList.getSize());
+
+        newList.addTask(newItem2);
+        assertEquals(2, newList.getSize());
+    }
+
+    @Test
+    public void removingTaskItemsDecreasesSize() {
+        TaskItem newItem1 = new TaskItem("Title", "Description", "01/10/2020");
+        TaskItem newItem2 = new TaskItem("Title", "Description", "01/10/2020");
+        TaskList newList = new TaskList();
+
+        newList.addTask(newItem1);
+        newList.addTask(newItem2);
+        assertEquals(2, newList.getSize());
+
+        newList.removeTask(1);
+        assertEquals(1, newList.getSize());
+    }
+
+    @Test
+    public void removingTaskItemsFailsWithInvalidIndex() {
+        TaskItem newItem1 = new TaskItem("Title", "Description", "01/10/2020");
+        TaskList newList = new TaskList();
+
+        newList.addTask(newItem1);
+        assertThrows(IndexOutOfBoundsException.class, () -> newList.removeTask(3));
+    }
+
+    @Test
+    public void editingTaskItemTitleChangesValue() {
+        TaskItem newItem = new TaskItem("Title", "Description", "01/10/2020");
+        TaskList newList = new TaskList();
+        newList.addTask(newItem);
+
+        TaskItem temp = newList.getItem(0);
+        assertEquals("Title", temp.getTitle());
+
+        newList.editTaskTitle(0, "New Title");
+
+        assertEquals("New Title", newList.getItemTitle(0));
+    }
+
+    @Test
+    public void editingTaskItemTitleFailsWithInvalidIndex() {
+        TaskItem newItem = new TaskItem("Title", "Description", "01/10/2020");
+        TaskList newList = new TaskList();
+        newList.addTask(newItem);
+
+        TaskItem temp = newList.getItem(0);
+        assertEquals("Title", temp.getTitle());
+
+        assertThrows(IndexOutOfBoundsException.class, () -> newList.editTaskTitle(2, "New Title"));
+    }
+
+    @Test
+    public void editingTaskItemDescriptionChangesValue() {
+        TaskItem newItem = new TaskItem("Title", "Description", "01/10/2020");
+        TaskList newList = new TaskList();
+        newList.addTask(newItem);
+
+        TaskItem temp = newList.getItem(0);
+        assertEquals("Description", temp.getDescription());
+
+        newList.editTaskDescription(0, "New Description");
+
+        assertEquals("New Description", newList.getItemDescription(0));
+    }
+
+    @Test
+    public void editingTaskItemDescriptionFailsWithInvalidIndex() {
+        TaskItem newItem = new TaskItem("Title", "Description", "01/10/2020");
+        TaskList newList = new TaskList();
+        newList.addTask(newItem);
+
+        TaskItem temp = newList.getItem(0);
+        assertEquals("Description", temp.getDescription());
+
+        assertThrows(IndexOutOfBoundsException.class, () -> newList.editTaskDescription(2, "New Description"));
+    }
+
+    @Test
+    public void editingTaskItemDueDateChangesValue() {
+        TaskItem newItem = new TaskItem("Title", "Description", "01/10/2020");
+        TaskList newList = new TaskList();
+        newList.addTask(newItem);
+
+        TaskItem temp = newList.getItem(0);
+        assertEquals("01/10/2020", temp.getDate());
+
+        newList.editTaskDate(0, "12/12/2020");
+
+        assertEquals("12/12/2020", newList.getItemDate(0));
+    }
+
+    @Test
+    public void editingTaskItemDueDateFailsWithInvalidIndex() {
+        TaskItem newItem = new TaskItem("Title", "Description", "01/10/2020");
+        TaskList newList = new TaskList();
+        newList.addTask(newItem);
+
+        TaskItem temp = newList.getItem(0);
+        assertEquals("01/10/2020", temp.getDate());
+
+        assertThrows(IndexOutOfBoundsException.class, () -> newList.editTaskDate(2, "12/12/2020"));
+    }
+
+    @Test
+    public void completingTaskItemChangesStatus() {
+        TaskItem newItem = new TaskItem("Title", "Description", "01/10/2020");
+        TaskList newList = new TaskList();
+        newList.addTask(newItem);
+
+        newList.editTaskCompletionStatusToDone(0);
+
+        assertTrue(newList.getItemCompletionStatus(0));
+    }
+
+    @Test
+    public void completingTaskItemFailsWithInvalidIndex() {
+        TaskItem newItem = new TaskItem("Title", "Description", "01/10/2020");
+        TaskList newList = new TaskList();
+        newList.addTask(newItem);
+
+        assertThrows(IndexOutOfBoundsException.class, () -> newList.editTaskCompletionStatusToDone(2));
+    }
+
+    @Test
+    public void uncompletingTaskItemChangesStatus() {
+        TaskItem newItem = new TaskItem("Title", "Description", "01/10/2020");
+        TaskList newList = new TaskList();
+        newList.addTask(newItem);
+
+        newList.editTaskCompletionStatusToNotDone(0);
+
+        assertFalse(newList.getItemCompletionStatus(0));
+    }
+
+    @Test
+    public void uncompletingTaskItemFailsWithInvalidIndex() {
+        TaskItem newItem = new TaskItem("Title", "Description", "01/10/2020");
+        TaskList newList = new TaskList();
+        newList.addTask(newItem);
+
+        assertThrows(IndexOutOfBoundsException.class, () -> newList.editTaskCompletionStatusToNotDone(2));
+    }
+
+    @Test
+    public void gettingTaskItemTitleSucceedsWithValidIndex() {
+        TaskItem newItem = new TaskItem("Title", "Description", "01/10/2020");
+        TaskList newList = new TaskList();
+        newList.addTask(newItem);
+
+        assertEquals("Title",newList.getItemTitle(0));
+    }
+
+    @Test
+    public void gettingTaskItemTitleFailsWithInvalidIndex() {
+        TaskItem newItem = new TaskItem("Title", "Description", "01/10/2020");
+        TaskList newList = new TaskList();
+        newList.addTask(newItem);
+
+        assertThrows(IndexOutOfBoundsException.class, () -> newList.getItemTitle(2));
+    }
+
+    @Test
+    public void gettingTaskItemDescriptionSucceedsWithValidIndex() {
+        TaskItem newItem = new TaskItem("Title", "Description", "01/10/2020");
+        TaskList newList = new TaskList();
+        newList.addTask(newItem);
+
+        assertEquals("Description", newList.getItemDescription(0));
+    }
+
+    @Test
+    public void gettingTaskItemDescriptionFailsWithInvalidIndex() {
+        TaskItem newItem = new TaskItem("Title", "Description", "01/10/2020");
+        TaskList newList = new TaskList();
+        newList.addTask(newItem);
+
+        assertThrows(IndexOutOfBoundsException.class, () -> newList.getItemDescription(2));
+    }
+
+    @Test
+    public void gettingTaskItemDueDateSucceedsWithValidIndex() {
+        TaskItem newItem = new TaskItem("Title", "Description", "01/10/2020");
+        TaskList newList = new TaskList();
+        newList.addTask(newItem);
+
+        assertEquals("01/10/2020", newList.getItemDate(0));
+    }
+
+    @Test
+    public void gettingTaskItemDueDateFailsWithInvalidIndex() {
+        TaskItem newItem = new TaskItem("Title", "Description", "01/10/2020");
+        TaskList newList = new TaskList();
+        newList.addTask(newItem);
+
+        assertThrows(IndexOutOfBoundsException.class, () -> newList.getItemDate(2));
+    }
+
+
+
+}
