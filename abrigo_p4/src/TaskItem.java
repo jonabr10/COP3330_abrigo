aaa61a20278706d8d429b733071a5d0db28565d8
jonabr10 <jonabrigo@knights.ucf.edu>
2020-11-14 23:22:27 -0500

    TaskItem and TaskItemTest done

--- abrigo_p4/src/TaskItem.java
+++ abrigo_p4/src/TaskItem.java
@@ -1,3 +1,139 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class TaskItem {
-    
+    boolean completionStatus;
+    String title;
+    String description;
+    String date;
+
+    public TaskItem (String title, String desc, String date) throws InvalidDateException {
+        this.completionStatus = false;
+
+        if (isTitleValid(title)) {
+            this.title = title;
+        } else {
+            throw new InvalidTitleException("Title is invalid, string length must be at least  1 character long");
+        }
+
+        if (isDescriptionValid(desc)) {
+            this.description = desc;
+        } else {
+            throw new InvalidDescriptionException("Description is invalid, string length must be at least 1 character long");
+        }
+
+        if (isDateValid(date)) {
+            this.date = date;
+        } else {
+            throw new InvalidDateException("Date is invalid, date format must be MM/DD/YYYY");
+        }
+    }
+
+    public TaskItem (boolean completionStatus, String title, String desc, String date) throws InvalidDateException {
+        this.completionStatus = completionStatus;
+
+        if (isTitleValid(title)) {
+            this.title = title;
+        } else {
+            throw new InvalidTitleException("Title is invalid, string length must be at least  1 character long");
+        }
+
+        if (isDescriptionValid(desc)) {
+            this.description = desc;
+        } else {
+            throw new InvalidDescriptionException("Description is invalid, string length must be at least 1 character long");
+        }
+
+        if (isDateValid(date)) {
+            this.date = date;
+        } else {
+            throw new InvalidDateException("Date is invalid, date format must be MM/DD/YYYY");
+        }
+    }
+
+    public void setCompletionStatusToDone() {
+        this.completionStatus = true;
+    }
+    public void setCompletionStatusToNotDone() {
+        this.completionStatus = false;
+    }
+    public void setTitle(String newTitle) {
+        if (isTitleValid(newTitle)) {
+            this.title = newTitle;
+        } else {
+            throw new InvalidTitleException("New title is invalid, string length must be at least  1 character long");
+        }
+    }
+    public void setDescription(String newDesc) {
+        if (isDescriptionValid(newDesc)) {
+            this.description = newDesc;
+        } else {
+            throw new InvalidDescriptionException("New description is invalid, string length must be at least  1 character long");
+        }
+    }
+    public void setDate(String newDate) throws InvalidDateException {
+        if (isDateValid(newDate)) {
+            this.date = newDate;
+        } else {
+            throw new InvalidDateException("Date is invalid, date format must be MM/DD/YYYY");
+        }
+    }
+
+    public boolean getCompletionStatus() {
+        return this.completionStatus;
+    }
+    public String getTitle() {
+        return this.title;
+    }
+    public String getDescription() {
+        return this.description;
+    }
+    public String getDate() {
+        return this.date;
+    }
+
+
+    private boolean isTitleValid(String title) {
+        return title.length() > 0;
+    }
+    private boolean isDescriptionValid(String description) {
+        return description.length() > 0;
+    }
+    private boolean isDateValid(String date) {
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+        Date dateObj = null;
+        df.setLenient(false);
+        // Note-to-self: setLenient(boolean leniency) method in DateFormat class is used to
+        //               specify whether the interpretation of the date and time of this
+        //               DateFormat object is to be lenient or not.
+        //               (source: geeksforgeeks.org/dateformat-setlenient-method-in-java-with-examples/)
+
+        try {
+            dateObj = df.parse(date);
+            // Note-to-self: df object will try to parse the String date to the specified date
+            //               formatter. If there is a format mismatch, then an exception is
+            //               thrown and false will be returned.
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
+
+class InvalidTitleException extends IllegalArgumentException {
+    public InvalidTitleException(String message) {
+        super(message);
+    }
+}
+class InvalidDescriptionException extends IllegalArgumentException {
+    public InvalidDescriptionException(String message) {
+        super(message);
+    }
+}
+class InvalidDateException extends IllegalArgumentException {
+    public InvalidDateException(String message) {
+        super(message);
+    }
 }
--- abrigo_p4/src/TaskItemTest.java
+++ abrigo_p4/src/TaskItemTest.java
@@ -4,7 +4,72 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TaskItemTest {
     @Test
-    
+    public void creatingTaskItemWithValidTitleDescriptionDate() {
+        assertDoesNotThrow(() -> new TaskItem("Title", "Description", "01/10/2020"));
+    }
 
+    @Test
+    public void creatingTaskItemWithInvalidTitleAndValidDescriptionDate() {
+        assertThrows(InvalidTitleException.class, () -> new TaskItem("", "Description", "01/10/2020"));
+    }
 
+    @Test
+    public void creatingTaskItemWithInvalidDescriptionAndValidTitleDate() {
+        assertThrows(InvalidDescriptionException.class, () -> new TaskItem("Title", "", "01/10/2020"));
+    }
+
+    @Test
+    public void creatingTaskItemWithInvalidDateFormatAndValidTitleDescription() {
+        assertThrows(InvalidDateException.class, () -> new TaskItem("Title", "Description", "01-10-2020"));
+    }
+
+    @Test
+    public void creatingTaskItemWithInvalidDateInputAndValidTitleDescription() {
+        assertThrows(InvalidDateException.class, () -> new TaskItem("Title", "Description", "Hot Sauce"));
+    }
+
+    @Test
+    public void settingTaskItemTitleSucceedsWithValidTitle() {
+        TaskItem test = new TaskItem("Title", "Description", "01/10/2020");
+        test.setTitle("NewTitle");
+
+        assertEquals("NewTitle", test.getTitle());
+    }
+
+    @Test
+    public void settingTaskItemTitleSucceedsWithInvalidTitle() {
+        TaskItem test = new TaskItem("Title", "Description", "01/10/2020");
+
+        assertThrows(InvalidTitleException.class, () -> test.setTitle(""));
+    }
+
+    @Test
+    public void settingTaskItemTitleSucceedsWithValidDescription() {
+        TaskItem test = new TaskItem("Title", "Description", "01/10/2020");
+        test.setDescription("NewDescription");
+
+        assertEquals("NewDescription", test.getDescription());
+    }
+
+    @Test
+    public void settingTaskItemTitleSucceedsWithInvalidDescription() {
+        TaskItem test = new TaskItem("Title", "Description", "01/10/2020");
+
+        assertThrows(InvalidDescriptionException.class, () -> test.setDescription(""));
+    }
+
+    @Test
+    public void settingTaskItemDueDateSucceedsWithValidDate() {
+        TaskItem test = new TaskItem("Title", "Description", "01/10/2020");
+        test.setDate("12/10/2020");
+
+        assertEquals("12/10/2020", test.getDate());
+    }
+
+    @Test
+    public void settingTaskItemDueDateFailsWithInvalidDate() {
+        TaskItem test = new TaskItem("Title", "Description", "01/10/2020");
+
+        assertThrows(InvalidDateException.class, () -> test.setDate("Hot Sauce"));
+    }
 }
